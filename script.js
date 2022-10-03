f4d60505b97b96d44595d464deddee843ed55a7c
sakiburrifat <rifatringku@gmail.com>
2022-10-04 00:06:21 +0600

    final update

--- script.js
+++ script.js
@@ -1,13 +1,52 @@
-setInterval(somoy, 1000)
 
-function somoy() {
-    const myDate = new Date();
-    const hour = myDate.getHours();
-    const minute = myDate.getMinutes();
-    const second = myDate.getSeconds();
-    const myWatch = document.getElementById('time');
-    hour <= 12 ? myWatch.innerText = hour + ":" + minute + ":" + second + "  AM" : myWatch.innerText = hour - 12 + ":" + minute + ":" + second + "  PM"
+
+function currentTime() {
+    var date = new Date();
+    var hour = date.getHours();
+    var min = date.getMinutes();
+    var sec = date.getSeconds();
+    hour = updateTime(hour);
+    min = updateTime(min);
+    sec = updateTime(sec);
+    hour > 12 ? document.getElementById("time").innerText = hour - 12 + " : " + min + " : " + sec + " PM" : document.getElementById("time").innerText = hour + " : " + min + " : " + sec + " AM";
+
+    setTimeout(function () { currentTime() }, 1000);
 }
+
+function updateTime(time) {
+    if (time < 10) {
+        return "0" + time;
+    }
+    else {
+        return time;
+    }
+}
+
+currentTime();
+
+
+
+
+
+
+
+
+
+
+
+// setInterval(somoy, 1000)
+
+// function somoy() {
+//     const myDate = new Date();
+//     const hour = myDate.getHours();
+//     const minute = myDate.getMinutes();
+//     const second = myDate.getSeconds();
+//     const myWatch = document.getElementById('time');
+//     hour <= 12 ? myWatch.innerText = hour + ":" + minute + ":" + second + "  AM" : myWatch.innerText = hour - 12 + ":" + minute + ":" + second + "  PM"
+// }
+
+
+
 
 
 
